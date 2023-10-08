773601bed206a6c6da2bf9499b874372225adbfc
AndrewJNg <andrew.joseph.ng@gmail.com>
2023-10-08 14:37:31 +0800

    added buzzer to the system, now going to test the buzzer

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ECE4078-Robot_code
-code for improving on commands that operate the Alphabot2 remotely over wifi
+
+This are modified code to enable the AlphaBot2 to operate it's buzzer and servo over wifi for ECE4078 
+Original code: https://github.com/clow0003/ECE4078_MY_Lab_2022
+
+
---
+++ listen.py
@@ -0,0 +1,46 @@
+import socket
+import cv2
+import time
+import numpy as np
+from AlphaBot import AlphaBot
+from picamera import PiCamera
+from bottle import Bottle, get, run, request
+
+app = Bottle()
+
+def setup_app(app):
+    app.bot = AlphaBot()
+    app.camera = PiCamera()
+    app.camera.resolution = (640,480)
+    app.camera.framerate = 24
+    app.camera.start_preview()
+    time.sleep(2)
+        
+setup_app(app)
+
+@app.get("/camera/get")
+def get_image():
+    width, height = app.camera.resolution
+    image = np.empty((height * width * 3,), dtype=np.uint8)
+    app.camera.capture(image, 'bgr', use_video_port=True)
+    image = image.reshape((height, width, 3))
+    return cv2.imencode('.jpg', image)[1].tobytes()
+    
+@app.get('/robot/set/velocity')
+def set_velocity():
+    vel = request.query.value.split(',')
+    app.bot.setMotor(float(vel[0]), float(vel[1]))
+    if request.query.time != '':
+        time_run = float(request.query.time)
+        time.sleep(time_run)
+        app.bot.stop()
+        
+@app.get('/buzzer/set')
+def set_buzzer():
+    buzz_logic = request.query.value.split(',')
+    app.bot.setBuzzer(bool(buzz_logic))
+
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.connect(('8.8.8.8',80))
+localhost=s.getsockname()[0]
+run(app, host=localhost, port=8000)
